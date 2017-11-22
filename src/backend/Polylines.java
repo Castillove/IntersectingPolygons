65a5cfbcebe41b5e8f7a48d5e61274fdc181e72c
Eyal Minsky-Fenick <eyjmfc@gmail.com>
2017-11-22 14:54:38 -0500

    Wrote the main algorithm.

--- src/backend/Polylines.java
+++ src/backend/Polylines.java
@@ -2,11 +2,14 @@ package backend;
 
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Polylines {
     @SuppressWarnings({ "unchecked", "rawtypes" })
     private final List<IPoint> blue = new ArrayList(), green = new ArrayList();
+    private Set<Point> cans = null;
     public void addPoint(boolean blue, int x, int y)
 	    throws CollinearPointException, VerticalPointsException {
 	IPoint p = new IPoint(x, y, blue? Color.BLUE : Color.GREEN);
@@ -65,19 +68,34 @@ public class Polylines {
 			pb.ix, pb.iy,
 			cur.ix, cur.iy,
 			b.ix, b.iy);
-	    if(prl.m.equals(bl.m))
-		throw new ParallelLinesException(
-			prev.ix, prev.iy,
-			cur.ix, cur.iy,
-			b.ix, b.iy,
-			pb.ix, pb.iy);
-	    if(nxl.m.equals(bl.m))
-		throw new ParallelLinesException(
-			next.ix, next.iy,
-			cur.ix, cur.iy,
-			b.ix, b.iy,
-			pb.ix, pb.iy);
 	    pb = b;
+	    cans = null;
 	}
+    }
+    public Set<Point> getIntersections() {
+	if(cans != null)
+	    return(cans);
+	cans = new HashSet<Point>();
+	List<Point> gcon = new ArrayList<Point>();
+	for(int i = 0; i < green.size(); i++)
+	    gcon.add(Geometry.dualize(Geometry.connection(green.get(i),
+		    green.get((i + 1) % blue.size()), null)));
+	for(int i = 0; i < blue.size(); i++) {
+	    Point thisb = blue.get(i);
+	    Point nextb = blue.get((i+1)%blue.size());
+	    Point bcon = Geometry.dualize(Geometry.connection(thisb, nextb, null)); 
+	    for(int j = 0; j < green.size(); j++) {
+		Line l = Geometry.connection(bcon, gcon.get(j), null);
+		if(l.m.IsInfinity())
+		    break;
+		if(l.m.compareTo(thisb.x) * l.m.compareTo(nextb.x) != -1)
+		    break;
+		if(l.m.compareTo(green.get(i).x) *
+			l.m.compareTo(green.get((i+1)%green.size()).x) != -1)
+		    break;
+		cans.add(Geometry.dualize(l));
+	    }
+	}
+	return(cans);
     }
 }
